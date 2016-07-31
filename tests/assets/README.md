759eb5a69e0a3224bde31efa4ac38d91d15b8080
William Gibb <williamgibb@gmail.com>
2016-07-31 00:40:14 -0400

    Add a test for the decoder

---
+++ tests/assets/README.md
@@ -0,0 +1,1 @@
+Some files in this directory may contain malicious VBE scripts.  Run these at your own risk!
---
+++ tests/test_decoder.py
@@ -0,0 +1,44 @@
+# XXX Fill out docstring!
+"""
+test_decoder.py from vbe_decoder
+Created: 07/30/16
+
+Purpose:
+
+Examples:
+
+Usage:
+
+"""
+# Stdlib
+from __future__ import print_function
+import logging
+import os
+import unittest
+# Third Party code
+# Custom Code
+import vbe_decoder
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s [%(filename)s:%(funcName)s]')
+log = logging.getLogger(__name__)
+
+
+ASSETS_PATH = os.path.join(os.path.split(__file__)[0], 'assets')
+
+
+class TestDecoder(unittest.TestCase):
+    def setUp(self):
+        self.decoder = vbe_decoder.decoder.Decoder()
+
+    def run_decoder(self, input_buf_fp, expected_buf_fp):
+        inbuf = open(input_buf_fp, 'rb').read()
+        outbuf = open(expected_buf_fp, 'rb').read()
+        self.decoder.decode(inbuf)
+        decoded_buf = self.decoder.output_buf
+        self.assertEqual(outbuf, decoded_buf)
+
+    def test_contrato(self):
+        log.info('Running contrato test')
+        infp = os.path.join(ASSETS_PATH, 'contrato.vbe.malware')
+        opfp = os.path.join(ASSETS_PATH, 'contrato.vbe.malware.decoded')
+        self.run_decoder(input_buf_fp=infp, expected_buf_fp=opfp)
